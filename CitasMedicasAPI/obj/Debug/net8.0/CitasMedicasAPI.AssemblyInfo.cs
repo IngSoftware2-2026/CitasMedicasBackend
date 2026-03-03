f7be7fda58adda8e16ae068ee6d5e043fcf7beaf
Christell Gamez Garcia <156734210+Chrissgamez@users.noreply.github.com>
2026-03-02 22:16:03 -0600

    Primer commit: Estructura inicial de CitasMedicasAPI

--- CitasMedicasAPI/obj/Debug/net8.0/CitasMedicasAPI.AssemblyInfo.cs
+++ CitasMedicasAPI/obj/Debug/net8.0/CitasMedicasAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasMedicasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de65b5d304f425af88b88c70659a4ccd74a987d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasMedicasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasMedicasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
