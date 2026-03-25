59e0470adf1abdd446aa7c78d04a4a8c260443df
Eric Urraco <gamezgarciachris@gmail.com>
2026-03-24 23:08:14 -0600

    Update ActualizarConsultaDTO.cs

--- CitasMedicas-Backend/CitasMedicas.Models/Models/ActualizarConsultaDTO.cs
+++ CitasMedicas-Backend/CitasMedicas.Models/Models/ActualizarConsultaDTO.cs
@@ -1,6 +1,5 @@
     public class ActualizarConsultaDto
     {
-        public int CitaId { get; set; }
         public int ConsultaId { get; set; }
         public string? Motivo { get; set; }
         public string? Notas { get; set; }
